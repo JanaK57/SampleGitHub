22ad628788b41e91de540c2b5a2394e1cd1bc2ae
jana5 <jana5793@gmail.com>
2022-08-31 21:40:50 +0530

    New Change

--- src/org/test/Sample.java
+++ src/org/test/Sample.java
@@ -14,6 +14,15 @@ for(int k=0;k<i+1;k++)
 	System.out.print("* ");
 }
 System.out.println();
-
 }
+System.out.println("---------------------------------");
+for(int i=0;i<7;i++)
+{
+	for(int j=0;j<i+1;j++)
+	{
+	System.out.print("* ");	
+	}
+	System.out.println();
+}
+
 }}
