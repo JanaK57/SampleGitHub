4bd0f8b699ca8a6335a551b3737e81b37793a290
jana5 <jana5793@gmail.com>
2022-08-31 21:00:48 +0530

    Jana intial commit

---
+++ src/org/test/Sample.java
@@ -0,0 +1,18 @@
+package org.test;
+
+public class Sample {
+public static void main(String[] args) {
+int num =8;
+for(int i=0;i<num;i++) 
+{
+for(int j=2*(num-i);j>=0;j--)
+{
+System.out.print(" ");
+}
+for(int k=0;k<i+1;k++)
+{
+	System.out.print("* ");
+}
+System.out.println();
+}
+}}
